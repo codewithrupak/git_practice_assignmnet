aa80a402baa6e9db2a6141a7767f32bc4133c317
codewithrupak <rupakdash999@gmail.com>
2022-11-03 18:17:19 +0530

    Check else stateent

---
+++ palindrome.js
@@ -0,0 +1,15 @@
+let string = "naman"
+
+function reverse(string){
+    let bag = "";
+    for (let x = string.length - 1; x >= 0; x--){
+        bag += string[x];
+
+    }return bag;
+}
+let reversed = reverse(string);
+if (string = reversed){
+    console.log("palindrome";)
+}
+}
+// add else condition
